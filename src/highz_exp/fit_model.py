bd195bcb983574dc97a20c535a35094f377ff4f5
Joy Zhou <yuntongzhou@outlook.com>
2025-08-04 09:40:06 -0400

    Add fit_s11_spectrum function for fitting measured S11 spectra to theoretical models

---
+++ src/highz_exp/fit_model.py
@@ -0,0 +1,65 @@
+from os.path import join as pjoin
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import skrf as rf
+from .unit_convert import *
+
+def fit_s11_spectrum(
+    measured_data: rf.Network,
+    theory_data: rf.Network,
+    gain_func,
+    extra_func,
+    p0,
+    n_gain_params,
+):
+    """
+    Fit the measured S11 spectrum to a model of the form:
+        corrected_mag = |theory_s11 * gain_func(f, ...) + extra_func(f, ...)|
+
+    Parameters:
+    - measured_data (rf.Network)
+    - theory_data (rf.Network)
+    - gain_func (callable): multiplicative correction, function of form (f, *params)
+    - extra_func (callable): additive correction, function of form (f, *params)
+    - p0 (list): initial guess for all params (gain + extra)
+    - n_gain_params (int): number of parameters in gain_func
+
+    Returns:
+    - popt (list): best-fit parameters
+    - pcov (2D array): covariance matrix
+    - corrected_ntwk (rf.Network): corrected theory spectrum
+    """
+    if not np.allclose(measured_data.f, theory_data.f):
+        raise ValueError("Frequency grids must match.")
+
+    f = measured_data.f
+    f_GHz = f / 1e9  # for numerical stability
+
+    theory_s11 = theory_data.s[:, 0, 0]
+    theory_phase = np.angle(theory_s11)
+    measured_mag = np.abs(measured_data.s[:, 0, 0])
+
+    # Fit wrapper
+    def fit_func(f, *params):
+        gain_params = params[:n_gain_params]
+        extra_params = params[n_gain_params:]
+        model = theory_s11 * gain_func(f, *gain_params) + extra_func(f, *extra_params)
+        return np.abs(model)
+
+    # Fit
+    popt, pcov = curve_fit(fit_func, f_GHz, measured_mag, p0=p0)
+
+    # Apply best-fit model
+    gain_params_opt = popt[:n_gain_params]
+    extra_params_opt = popt[n_gain_params:]
+    corrected_s11 = theory_s11 * gain_func(f_GHz, *gain_params_opt) + extra_func(f_GHz, *extra_params_opt)
+
+    # Return new Network
+    corrected_ntwk = rf.Network()
+    corrected_ntwk.f = theory_data.f
+    corrected_ntwk.s = corrected_s11.reshape(-1, 1, 1)
+    corrected_ntwk.z0 = theory_data.z0
+    corrected_ntwk.frequency = theory_data.frequency
+
+    return popt, pcov, corrected_ntwk
