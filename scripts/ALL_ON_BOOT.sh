c9106b5118674c02c81b110fd337cc15c8c948e3
Abdulla Alhosani <ama1029@nyu.edu>
2025-09-17 15:59:38 -0400

    Add scripts directory with mounting, unmounting, and spectrometer startup scripts

---
+++ scripts/ALL_ON_BOOT.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# /home/peterson/ALL_ON_BOOT.sh
+
+LOGDIR="/home/peterson/logs"
+mkdir -p "$LOGDIR"
+
+echo "[$(date)] Starting Digital Spectrometer Setup..." >> "$LOGDIR/digital_spec.log"
+/home/peterson/Desktop/sandbox-CASPER/highz_switch/FILE_SWITCHING_2025/launcher.sh >> "$LOGDIR/digital_spec_No_HPLP_two_shorts_cronlog_$(date +\%Y-\%m-\%d_\%H-\%M-\%S).log" 2>&1
+
+#echo "[$(date)] Starting Daemon Setup..." >> "$LOGDIR/daemon_setup.log"
+#/home/peterson/launcher_Daemon_Setup.sh >> "$LOGDIR/daemon_setup.log" 2>&1
+
+#echo "[$(date)] Starting FB process..." >> "$LOGDIR/fb.log"
+#/home/peterson/launcher_FB.sh >> "$LOGDIR/fb.log" 2>&1
+
+echo "[$(date)] All startup processes complete." >> "$LOGDIR/master.log"
