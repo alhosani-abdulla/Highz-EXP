1c3ee21ef4b6b7172e7abedfa87e5797207f4441
Joy Zhou <yuntongzhou@outlook.com>
2025-09-29 11:08:26 -0400

    Enhance interpolate_ntwk_dict function: improve frequency range handling with warnings for out-of-bounds requests and ensure clipping to network-specific ranges.

--- scripts/highz_alias.sh
+++ scripts/highz_alias.sh
@@ -2,3 +2,7 @@
 alias TakeSpecs='python ~/Highz-EXP/src/digital_spectrometer/run_spectrometer.py'
 alias ViewSpecs='python ~/Desktop/RTV_nosave_V4.py'
 alias GetSpecs='cd /media/peterson/INDURANCE'
+
+CreatePlot() {
+    python ~/Highz-EXP/src/digital_spectrometer/image_creator.py "$@"
+}
