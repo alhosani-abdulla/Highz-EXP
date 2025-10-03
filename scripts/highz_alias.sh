cd5764a823a65e549070ccba577dec852a9da12b
Joy Zhou <yuntongzhou@outlook.com>
2025-10-03 11:04:56 -0400

    Add alias for TestView to streamline spectrum viewing process

--- scripts/highz_alias.sh
+++ scripts/highz_alias.sh
@@ -2,6 +2,7 @@
 alias TakeSpecs='python ~/Highz-EXP/src/digital_spectrometer/run_spectrometer.py'
 alias ViewSpecs='python ~/Desktop/RTV_nosave_V4.py'
 alias GetSpecs='cd /media/peterson/INDURANCE'
+alias TestView='python ~/Highz-EXP/src/digital_spectrometer/viewspec_now.py'
 
 export PYTHONPATH=~/Highz-EXP/src:$PYTHONPATH
 
