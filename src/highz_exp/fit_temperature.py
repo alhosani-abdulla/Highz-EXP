0c05dba62db9f1f5d25ee9b6c9ce655966235f89
Joy Zhou <yuntongzhou@outlook.com>
2025-11-17 09:15:23 -0800

    Enhance: Add RBW parameter to Y_Factor_Thermoeter initialization and implement gain_wo_cal method for gain calculation without calibration

--- src/highz_exp/fit_temperature.py
+++ src/highz_exp/fit_temperature.py
@@ -7,7 +7,7 @@ class Y_Factor_Thermoeter:
     """
     Class to handle Y-Factor temperature measurements and calculations.
     """
-    def __init__(self, DUT_hot, DUT_cold, DUT_name, T_hot, T_cold, cal_hot=None, cal_cold=None):
+    def __init__(self, DUT_hot, DUT_cold, DUT_name, T_hot, T_cold, cal_hot=None, cal_cold=None, RBW=None):
         """
         Initialize with DUT hot and cold spectra. Both in units of milliwatt.
 
@@ -20,12 +20,14 @@ class Y_Factor_Thermoeter:
             - T_cold (float): Cold source temperature in Kelvin.
             - cal_hot (np.ndarray, optional): Calibration spectrum without DUT at hot source temperature.
             - cal_cold (np.ndarray, optional): Calibration spectrum without DUT at cold source temperature
+            - RBW (float, optional): Resolution Bandwidth in Hz, required if no calibration spectra provided.
         """
         self.DUT_hot = np.array(DUT_hot)
         self.DUT_cold = np.array(DUT_cold)
         self.Y_factor = self.DUT_hot / self.DUT_cold
         self.CAL_hot = cal_hot
         self.CAL_cold = cal_cold
+        self.RBW = RBW
         self.label = DUT_name
         self.T_sys = self.compute_system_temperature(self.Y_factor, T_hot, T_cold)
         if self.CAL_hot is not None and self.CAL_cold is not None:
@@ -34,6 +36,8 @@ class Y_Factor_Thermoeter:
             self.T_dut = self.DUT_temp_with_cal(self.T_cal, self.g, self.T_sys)
         else:
             self.g = None # Gain will be calculated later
+            if self.RBW is not None:
+                self.g = self.gain_wo_cal(self.DUT_hot, self.DUT_cold, T_hot, T_cold, self.RBW)
             self.T_dut = None
             self.T_cal = None
     
@@ -69,6 +73,24 @@ class Y_Factor_Thermoeter:
         return T_sys
     
     @staticmethod
+    def gain_wo_cal(DUT_hot, DUT_cold, T_hot, T_cold, RBW) -> np.ndarray:
+        """
+        Calculation of DUT gain without calibration spectra at two source noise temperatures.
+
+        Parameters:
+            - DUT_hot (np.ndarray): Measured spectrum with DUT connected at hot source temperature.
+            - DUT_cold (np.ndarray): Measured spectrum with DUT connected at cold source temperature.
+            - T_hot (float): Hot source temperature in Kelvin.
+            - T_cold (float): Cold source temperature in Kelvin.
+            - RBW (float): Resolution Bandwidth in Hz.
+
+        Returns:
+            - g_dut (np.ndarray): Inferred gain of the DUT at each frequency in db scale.
+        """
+        g_dut = 10 * np.log10((DUT_hot - DUT_cold) / (T_hot - T_cold) * k_B / RBW)
+        return g_dut
+    
+    @staticmethod
     def gain_with_cal(DUT_hot, DUT_cold, cal_hot, cal_cold) -> np.ndarray:
         """
         Calculation of DUT gain with the second-stage/instrument calibration spectra without DUT at two source noise temperatures.
@@ -96,7 +118,7 @@ class Y_Factor_Thermoeter:
         return T_dut
     
     @staticmethod
-    def plot_temps(faxis, temp_values, labels, start_freq=10, end_freq=400,
+    def plot_temps(faxis, temp_values, labels, start_freq=10, end_freq=400, ymax=None,
                      title="DUT Temperature", xlabel="Frequency (MHz)", ylabel="temperature (Kelvin)", save_path=None):
         """
         Plot temperature of an component (referred to INPUT of the LNA) curves based on fitted line parameters.
@@ -120,6 +142,8 @@ class Y_Factor_Thermoeter:
         # Add a vertical marker at the starting frequency
         # plt.axvline(x=faxis[start_idx], color='red', linestyle='--', alpha=0.7,
         #            label=f'Start: {faxis[start_idx]} MHz')
+        if ymax is not None:
+            plt.ylim(0, ymax)
 
         plt.xlabel(xlabel, fontsize=20)
         plt.ylabel(ylabel, fontsize=20)
